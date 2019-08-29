60792c2746edb99ca57869d04bf722ef8aa1b41e
Graham <grahamcolmer@Grahams-MacBook-Pro.local>
2019-08-29 14:46:42 +0100

    Create, Read

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component }  from 'react';
+import {API,graphqlOperation} from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import {createNote, deleteNote} from './graphql/mutations';
+import {listNotes} from './graphql/queries';
 
-function App() {
+class App extends Component {
+
+  state = {
+    notes: []
+  }
+
+  async componentDidMount() {
+    const result = await API.graphql(graphqlOperation(listNotes));
+    this.setState({notes: result.data.listNotes.items})
+  }
+
+  handleChangeNote = event => this.setState({ note : event.target.value })
+
+  handleAddNote = async event => {
+    const {note, notes} = this.state;
+    event.preventDefault();
+
+    const input = {note}
+
+    const result = await API.graphql(graphqlOperation(createNote, {input} ))
+    const newNote = result.data.createNote;
+    const updatedNotes = [newNote,...notes];
+    this.setState({notes: updatedNotes, note: ""});
+  }
+
+  handleDeleteNote = async noteId => {
+    const {notes} = this.state;
+    const input = { id: noteId };
+    const result = await API.graphql(graphqlOperation(deleteNote, {input}));
+    const deletedNoteId = result.data.deleteNote.id;
+    const updatedNotes = notes.filter(note => note.id !== deletedNoteId);
+    this.setState({notes: updatedNotes})
+  }
+
+  render(){
+  const {notes, note} = this.state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">Amplify Notetaker</h1>
+      <form className="mb3" onSubmit={this.handleAddNote}>
+        <input className="pa2 f4" type="text" placeholder="Write your note" onChange={this.handleChangeNote} value={note}/>
+        <button className="pa2 f4" type="submit">Add Note</button>
+      </form>
+      <div>
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">
+              {item.note}
+            </li>
+            <button onClick={() => this.handleDeleteNote(item.id)} className="bg-transparent bn f4">
+              <span>&times;</span>
+             </button>
+          </div>  
+        ))}
+      </div>
     </div>
   );
 }
+}
 
-export default App;
+export default withAuthenticator(App ,{includeGreetings: true});
